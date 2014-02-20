f9995ff9f1db8cf0292a0f48e57469b4a99586e8
vilmar23 <vilmar_mcb@hotmail.com>
2014-02-20 15:40:47 +0100

    Carga inicial

---
+++ www/PhoneListener.js
@@ -0,0 +1,18 @@
+var PhoneListener = {
+    start: function(successCallback, failureCallback) {
+      return cordova.exec(
+        successCallback,
+        failureCallback,
+        'PhoneListener',
+        'startMonitoringPhoneState',
+      []); // no arguments required
+    },
+    stop: function(successCallback, failureCallback) {
+      return cordova.exec(
+        successCallback,
+        failureCallback,
+        'PhoneListener',
+        'stopMonitoringPhoneState',
+      []); // no arguments required
+    }
+};
