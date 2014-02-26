08150b4c8c6a9de2e56731f3e320f8fd770a6e02
vilmar23 <vilmar_mcb@hotmail.com>
2014-02-26 18:02:16 +0100

    Cambios para hacerlo funcionar

--- src/android/PhoneListener.java
+++ src/android/PhoneListener.java
@@ -18,25 +18,31 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.telephony.TelephonyManager;
 import android.util.Log;
 
-import org.apache.cordova.api.CordovaInterface;
-import org.apache.cordova.api.Plugin;
-import org.apache.cordova.api.PluginResult;
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CordovaWebView;
+import org.apache.cordova.PluginResult;
+import org.apache.cordova.networkinformation.NetworkManager;
 
 /**
 * @author Tommy-Carlos Williams,
 * Huge chunks lifted/adapted from the NextworkManager core PhoneGap plugin
 */
-public class PhoneListener extends Plugin {
+public class PhoneListener extends CordovaPlugin {
 
     public static final String TYPE_NONE = "NONE";
     private static final String LOG_TAG = "PhoneListener";
 
-    private String phoneListenerCallbackId;
+    private CallbackContext phoneListenerCallbackId;
 
     BroadcastReceiver receiver;
+    private boolean registered = false;
 
     /**
     * Constructor.
@@ -45,17 +51,10 @@ public class PhoneListener extends Plugin {
         this.receiver = null;
     }
 
-    /**
-    * Sets the context of the Command. This can then be used to do things like
-    * get file paths associated with the Activity.
-    *
-    * @param ctx The context of the main Activity.
-    */
-    public void setContext(CordovaInterface ctx) {
-        super.setContext(ctx);
-        this.phoneListenerCallbackId = null;
+    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+        super.initialize(cordova, webView);
 
-        // We need to listen to connectivity events to update navigator.connection
+        this.phoneListenerCallbackId = null;
         IntentFilter intentFilter = new IntentFilter() ;
         intentFilter.addAction(TelephonyManager.ACTION_PHONE_STATE_CHANGED);
         if (this.receiver == null) {
@@ -89,8 +88,11 @@ public class PhoneListener extends Plugin {
             };
             // register the receiver... this is so it doesn't have to be added to AndroidManifest.xml
             cordova.getActivity().registerReceiver(this.receiver, intentFilter);
+            this.registered = true;
         }
+
     }
+
 
     /**
     * Create a new plugin result and send it back to JavaScript
@@ -101,50 +103,52 @@ public class PhoneListener extends Plugin {
       if (this.phoneListenerCallbackId != null) {
         PluginResult result = new PluginResult(PluginResult.Status.OK, phoneState);
         result.setKeepCallback(keepCallback);
-        this.success(result, this.phoneListenerCallbackId);
+        this.phoneListenerCallbackId.sendPluginResult(result);
       }
+      webView.postMessage("networkconnection", phoneState);
     }
+
 
     /* (non-Javadoc)
     * @see com.phonegap.api.Plugin#execute(java.lang.String, org.json.JSONArray, java.lang.String)
     */
     @Override
-    public PluginResult execute(String action, JSONArray args, String callbackId) {
-        PluginResult.Status status = PluginResult.Status.INVALID_ACTION;
-        String result = "Unsupported Operation: " + action;
-        // either start or stop the listener...
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) {
+
         if (action.equals("startMonitoringPhoneState")) {
-          if (this.phoneListenerCallbackId != null) {
-                   return new PluginResult(PluginResult.Status.ERROR, "Phone listener already running.");
-                   }
-          this.phoneListenerCallbackId = callbackId;
+          this.phoneListenerCallbackId = callbackContext;
           PluginResult pluginResult = new PluginResult(PluginResult.Status.NO_RESULT);
           pluginResult.setKeepCallback(true);
-          return pluginResult;
+          callbackContext.sendPluginResult(pluginResult);
+          return true;
         }
         else if (action.equals("stopMonitoringPhoneState")) {
             removePhoneListener();
-                    this.updatePhoneState("", false); // release status callback
-                    this.phoneListenerCallbackId = null;
-                    return new PluginResult(PluginResult.Status.NO_RESULT);
+            this.updatePhoneState("", false); // release status callback
+            this.phoneListenerCallbackId = null;
+            PluginResult pluginResult = new PluginResult(PluginResult.Status.NO_RESULT);
+            callbackContext.sendPluginResult(pluginResult);
         }
 
-        return new PluginResult(status, result); // no valid action called
+        return false; // no valid action called
     }
+
 
     /**
     * Stop the phone listener receiver and set it to null.
     */
-        private void removePhoneListener() {
-            if (this.receiver != null) {
-                try {
-                    this.ctx.getContext().unregisterReceiver(this.receiver);
-                    this.receiver = null;
-                } catch (Exception e) {
-                    Log.e(LOG_TAG, "Error unregistering phone listener receiver: " + e.getMessage(), e);
-                }
+    private void removePhoneListener() {
+        if (this.receiver != null && this.registered) {
+            try {
+            	this.cordova.getActivity().unregisterReceiver(this.receiver);
+                this.registered = false;
+                this.receiver = null;
+            } catch (Exception e) {
+                Log.e(LOG_TAG, "Error unregistering phone listener receiver: " + e.getMessage(), e);
             }
         }
+    }
+
 
     /**
     * Stop phone listener receiver.
