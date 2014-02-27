9a5ac28122e7228a26d4095e3735fbda1279c6af
vilmar23 <vilmar_mcb@hotmail.com>
2014-02-27 11:37:12 +0100

    lo unimos a window.Phonelistener

--- www/PhoneListener.js
+++ www/PhoneListener.js
@@ -1,18 +1,31 @@
-var PhoneListener = {
-    start: function(successCallback, failureCallback) {
-      return cordova.exec(
+var exec = require('cordova/exec');
+
+var PhoneListener = function(src, successCallback, errorCallback, statusCallback) {
+
+};
+
+/**
+ * Start Listener
+ */
+PhoneListener.prototype.start = function(successCallback, failureCallback) {
+    exec(
         successCallback,
         failureCallback,
         'PhoneListener',
         'startMonitoringPhoneState',
-      []); // no arguments required
-    },
-    stop: function(successCallback, failureCallback) {
-      return cordova.exec(
+      []);
+};
+
+/**
+ * Stop Listener.
+ */
+PhoneListener.prototype.stop = function() {
+    exec(
         successCallback,
         failureCallback,
         'PhoneListener',
         'stopMonitoringPhoneState',
-      []); // no arguments required
-    }
+      []);
 };
+
+module.exports = PhoneListener;
